7fe57262463f828984284df2510ee68e40519826
yuliyang <645644717@qq.com>
2019-02-27 18:03:21 +0800

    添加正则

--- app/src/test/java/com/yuliyang/well_design/ExampleUnitTest.kt
+++ app/src/test/java/com/yuliyang/well_design/ExampleUnitTest.kt
@@ -20,10 +20,10 @@ class ExampleUnitTest {
 //        println(calendar.get(Calendar.DAY_OF_WEEK))
 //        calendar.updateStartDateForMonth()
 //        println("${calendar.get(Calendar.MONTH)}  ${calendar.get(Calendar.DAY_OF_MONTH)}")
-        val pattern = Pattern.compile("yuliyang(?=[12])")
-        val matches = pattern.matcher("yuliyang1yuliyang1");
-        while (matches.find()) {
-            println(matches.group(0))
+        val pattern = "yuliyang(?=[12])".toRegex()
+        val matches = pattern.findAll("yuliyang1yuliyang1")
+        matches.iterator().forEach {
+            println(it.value)
         }
     }
 }
